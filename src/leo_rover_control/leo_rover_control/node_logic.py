7f5bd589974e3609742cd80b5e16eb462c9a0465
arda <ardacihan7452@gmail.com>
2026-05-10 22:38:34 +0200

    Dummy wasd controller

--- src/leo_rover_control/leo_rover_control/node_logic.py
+++ src/leo_rover_control/leo_rover_control/node_logic.py
@@ -1,3 +1,5 @@
+# ~/PycharmProjects/leo_rover_gazebo/src/leo_rover_control/leo_rover_control/node_logic.py
+
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
@@ -7,51 +9,93 @@ import tty
 import select
 
 
-class KeyboardControlNode(Node):
-    def __init__(self):
+class KeyboardControl(Node):
+    def __init__(self, robot_namespace='leo1'):
         super().__init__('keyboard_control')
-        self.publisher = self.create_publisher(Twist, 'cmd_vel', 10)
-        self.get_logger().info('Keyboard control active. Use WASD keys to move robot. Press q to quit.')
+
+        # Check if namespace is provided and properly formatted
+        if robot_namespace and not robot_namespace.startswith('/'):
+            robot_namespace = '/' + robot_namespace
+
+        self.cmd_vel_topic = f'{robot_namespace}/cmd_vel' if robot_namespace else 'cmd_vel'
+
+        self.publisher = self.create_publisher(Twist, self.cmd_vel_topic, 10)
+
+        self.get_logger().info(f'=== Keyboard Teleop Ready ===')
+        self.get_logger().info(f'Publishing to: {self.cmd_vel_topic}')
+        self.get_logger().info('Controls:')
+        self.get_logger().info('  w - forward')
+        self.get_logger().info('  s - backward')
+        self.get_logger().info('  a - turn left')
+        self.get_logger().info('  d - turn right')
+        self.get_logger().info('  space - stop')
+        self.get_logger().info('  q - quit')
+
+        self.linear_speed = 0.5
+        self.angular_speed = 1.0
 
     def get_key(self):
+        """Get keyboard input without waiting for enter"""
         settings = termios.tcgetattr(sys.stdin)
         tty.setraw(sys.stdin.fileno())
-        select.select([sys.stdin], [], [], 0)
-        key = sys.stdin.read(1) if select.select([sys.stdin], [], [], 0)[0] else None
+        rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
+        key = None
+        if rlist:
+            key = sys.stdin.read(1)
         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
         return key
 
     def run(self):
+        twist = Twist()
+
         while rclpy.ok():
             key = self.get_key()
-            twist = Twist()
 
             if key == 'w':
-                twist.linear.x = 0.5
-                self.get_logger().info('Moving forward')
+                twist.linear.x = self.linear_speed
+                twist.angular.z = 0.0
+                self.get_logger().info(f'Moving forward: {self.linear_speed} m/s')
             elif key == 's':
-                twist.linear.x = -0.5
-                self.get_logger().info('Moving backward')
+                twist.linear.x = -self.linear_speed
+                twist.angular.z = 0.0
+                self.get_logger().info(f'Moving backward: {self.linear_speed} m/s')
             elif key == 'a':
-                twist.angular.z = 0.5
-                self.get_logger().info('Turning left')
+                twist.linear.x = 0.0
+                twist.angular.z = self.angular_speed
+                self.get_logger().info(f'Turning left: {self.angular_speed} rad/s')
             elif key == 'd':
-                twist.angular.z = -0.5
-                self.get_logger().info('Turning right')
-            elif key == 'q':
-                self.get_logger().info('Quitting')
-                break
-            else:
+                twist.linear.x = 0.0
+                twist.angular.z = -self.angular_speed
+                self.get_logger().info(f'Turning right: {self.angular_speed} rad/s')
+            elif key == ' ':
                 twist.linear.x = 0.0
                 twist.angular.z = 0.0
+                self.get_logger().info('Stopping')
+            elif key == 'q':
+                self.get_logger().info('Quitting...')
+                break
+            else:
+                # Keep publishing the last command or stop if no key
+                if key is None:
+                    pass  # Continue publishing current twist
+                else:
+                    continue
 
             self.publisher.publish(twist)
-            rclpy.spin_once(self, timeout_sec=0.1)
+            rclpy.spin_once(self, timeout_sec=0)
 
 
 def main(args=None):
     rclpy.init(args=args)
-    node = KeyboardControlNode()
+
+    # Parse namespace from command line
+    robot_ns = 'leo1'  # default
+    for i, arg in enumerate(sys.argv):
+        if arg == '--namespace' and i + 1 < len(sys.argv):
+            robot_ns = sys.argv[i + 1]
+            break
+
+    node = KeyboardControl(robot_ns)
     try:
         node.run()
     except KeyboardInterrupt:
--- src/leo_rover_control/setup.py
+++ src/leo_rover_control/setup.py
@@ -1,3 +1,5 @@
+# ~/PycharmProjects/leo_rover_gazebo/src/leo_rover_control/setup.py
+
 from setuptools import setup
 import os
 from glob import glob
@@ -9,19 +11,22 @@ setup(
     version='0.0.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        # Include launch files
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='arda',
-    description='Control logic for Leo Rover',
-    license='MIT',
+    maintainer='todo',
+    maintainer_email='todo@todo.todo',
+    description='Keyboard control for Leo Rover',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'intelligence_node = leo_rover_control.node_logic:main'
+            'node_logic = leo_rover_control.node_logic:main',
+            'keyboard_control = leo_rover_control.node_logic:main',
         ],
     },
 )
