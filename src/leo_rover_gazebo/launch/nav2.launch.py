5b9c8477e5cc587e01c0cf0074a7e1b7c2d4db8b
arda <ardacihan7452@gmail.com>
2026-05-24 22:33:47 +0200

    Trying to add costmaps

--- src/leo_rover_gazebo/launch/nav2.launch.py
+++ src/leo_rover_gazebo/launch/nav2.launch.py
@@ -3,11 +3,11 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import Node
+
 
 def generate_launch_description():
     pkg_nav2_bringup = get_package_share_directory('nav2_bringup')
-    pkg_gazebo       = get_package_share_directory('leo_rover_gazebo')
+    pkg_gazebo = get_package_share_directory('leo_rover_gazebo')
 
     params_file = os.path.join(pkg_gazebo, 'config', 'nav2_params_leo.yaml')
 
@@ -18,16 +18,8 @@ def generate_launch_description():
         launch_arguments={
             'use_sim_time': 'true',
             'params_file': params_file,
+            'bond_timeout': '0.0',
         }.items(),
     )
 
-    cmd_vel_relay = Node(
-        package='topic_tools',
-        executable='relay',
-        name='cmd_vel_relay',
-        arguments=['/cmd_vel_smoothed', '/leo1/cmd_vel'],
-        parameters=[{'use_sim_time': True}],
-        output='screen',
-    )
-
-    return LaunchDescription([nav2, cmd_vel_relay])
+    return LaunchDescription([nav2])
